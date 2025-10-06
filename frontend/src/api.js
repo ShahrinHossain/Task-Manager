22556e2854afb374883003dffba8070f15387351
ShahrinHossain <shahrin@iut-dhaka.edu>
2025-10-06 15:05:12 +0600

    removed frontend

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-// src/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:8000", // make sure your FastAPI backend is running here
-});
-
-export default api;
